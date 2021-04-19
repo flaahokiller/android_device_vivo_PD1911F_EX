f4b503897443bfadbb30adf8c4ef98765fc6c702
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-19 16:18:03 +0530

    Update omni_PD1911F_EX.mk

--- omni_PD1911F_EX.mk
+++ omni_PD1911F_EX.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := PD1911F_EX
+PRODUCT_RELEASE_NAME := PD1928BF_EX
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -11,10 +11,10 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := vivo
-PRODUCT_DEVICE := PD1911F_EX
+PRODUCT_DEVICE := PD1928BF_EX
 PRODUCT_MANUFACTURER := vivo
-PRODUCT_MODEL := vivo 1951
-PRODUCT_NAME := omni_PD1911F_EX
+PRODUCT_MODEL := vivo 1916
+PRODUCT_NAME := omni_PD1928BF_EX
 
 # enable stock zip packages flash
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
