5b487e061aab189feb6c80c9f15d7f6d1f4434c9
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-19 16:19:25 +0530

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter PD1911F_EX,$(TARGET_DEVICE)),)
+ifneq ($(filter PD1928BF_EX,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
